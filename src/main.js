1cb20cf9d944732562df5067b74164011945a029
Vimax <it.vimax@gmail.com>
2019-08-21 00:52:43 +0300

    # 4 video 1-1 time 0:32:10

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-((a) => {
-    console.log(a);
-})(1);
+import React from 'react';
+import ReactDom from 'react-dom';
+
+let hr = React.createElement('hr');
+let div = React.createElement('div', {className: 'test'}, [hr]);
+
+ReactDom.render(div, document.querySelector('#app'));
