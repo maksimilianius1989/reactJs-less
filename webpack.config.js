fb8779023e9bd8a9a73b8d735669f190b3582f56
Vimax <it.vimax@gmail.com>
2019-08-21 00:30:17 +0300

    # 3 video 1-1 time 0:22:15

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ let conf = {
         path: path.resolve(__dirname, './dist/'), // абсолютный путь к файлу
         filename: "main.js", // сам файл
         publicPath: "dist/" // что бы система в режиме dev понимала, где лежит js файл
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // если система видет файл, то применяется bable-loader
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 };
 
