ed02adb9d665eeb09348b64f301864d010a479bf
Vimax <it.vimax@gmail.com>
2019-08-21 21:03:18 +0300

    Подключение jsx

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-let hr = React.createElement('hr');
-let div = React.createElement('div', {className: 'test'}, [hr]);
+// let hr = React.createElement('hr');
+// let div = React.createElement('div', {className: 'test'}, [hr]);
 
-ReactDom.render(div, document.querySelector('#app'));
+ReactDom.render(<div className="test">
+    <span>Hello!</span>
+    <hr/>
+</div>, document.querySelector('#app'));
