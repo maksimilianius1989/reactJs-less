15c06615a68b7d43b0f37d6c25250e347471df1f
Bukach_M <maksym.bukach@volia.com>
2019-09-13 14:50:32 +0300

    less: 7.2 :: Time: 20

---
+++ src/api/cart.js
@@ -0,0 +1,12 @@
+import makeRequest from './helpers/makeRequest'
+
+function load(token) {
+    let url = 'cart/load.php'
+    if (token !== null) {
+        url += '?token=' + token
+    }
+
+    return makeRequest(url)
+}
+
+export {load}
